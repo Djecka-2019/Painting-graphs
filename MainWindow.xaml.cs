2877ce896664612d7a3ae9c841fbd2f5c1db1c7d
Djecka-2019 <56789849+Djecka-2019@users.noreply.github.com>
2024-05-24 16:20:17 +0300

    added event of button click

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -50,13 +50,79 @@ public partial class MainWindow : Window
     
     private void StartButton_Click(object sender, RoutedEventArgs e)
     {
-        var methodOfGraphInput = MethodOfGraphInput.SelectedIndex;
-        var methodOfColorInput = MethodOfColorInput.SelectedIndex;
-        var colorLimit = ColorLimitCheckbox.IsChecked;
-        var colorInput = ColorInput.Text;
-        if(methodOfGraphInput == 0)
+        var graphInput = GraphInput.Text;
+        List<int> numbers = graphInput.Split(' ').Select(int.Parse).ToList();
+        if (MethodOfGraphInput.SelectedIndex == 0)
         {
-            
+            int size = numbers[0];
+            List<List<int>> graph = new List<List<int>>();
+            for (int i = 0; i < size; i++)
+            {
+                graph.Add(new List<int>());
+                for (int j = 0; j < size; j++)
+                {
+                    graph[i].Add(numbers[i * size + j + 1]);
+                }
+            }
+            for(int i = 0 ; i < size ; i++)
+            {
+                for(int j = 0 ; j < size ; j++)
+                {
+                    Console.Write(graph[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Graph g = new Graph(graph, size);
+            if (MethodOfColorInput.SelectedIndex == 0)
+            {
+                List<int> result = g.GreedyPaint();
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine(i + " : " + result[i]);
+                }
+            }
+            else
+            {
+                List<int> result = g.ColorGraphWithMRVAndHeuristic();
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine(i + " : " + result[i]);
+                }
+            }
+        }
+        else
+        {
+            int size = numbers[0];
+            numbers.RemoveAt(0);
+            int amountOfPairs = numbers[0];
+            numbers.RemoveAt(0);
+            List<Tuple<int, int> > pairs = new List<Tuple<int, int>>();
+            for (int i = 0; i < amountOfPairs; i++)
+            {
+                pairs.Add(new Tuple<int, int>(numbers[i * 2], numbers[i * 2 + 1]));
+            }
+            Console.WriteLine(size);
+            for (int i = 0; i < amountOfPairs; i++)
+            {
+                Console.WriteLine(pairs[i].Item1 + " " + pairs[i].Item2);
+            }
+            Graph g = new Graph(pairs, size);
+            if (MethodOfColorInput.SelectedIndex == 0)
+            {
+                List<int> result = g.GreedyPaint();
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine(i + " : " + result[i]);
+                }
+            }
+            else
+            {
+                List<int> result = g.ColorGraphWithMRVAndHeuristic();
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine(i + " : " + result[i]);
+                }
+            }
         }
     }
 }
