4172cdd4087b3ffa7e9c1f702aa3ad88b7b2e43b
Djecka-2019 <56789849+Djecka-2019@users.noreply.github.com>
2024-05-29 10:08:24 +0300

    added input validation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,7 +8,18 @@ public partial class MainWindow
     {
         InitializeComponent();
         MethodOfGraphInput.SelectionChanged += MethodOfGraphInput_SelectionChanged;
-        
+        ColorLimitCheckbox.Checked += ColorLimitCheckbox_Checked;
+        ColorLimitCheckbox.Unchecked += ColorLimitCheckbox_Unchecked;
+    }
+    
+    private void ColorLimitCheckbox_Checked(object sender, RoutedEventArgs e)
+    {
+        ColorInput.IsEnabled = true;
+    }
+    
+    private void ColorLimitCheckbox_Unchecked(object sender, RoutedEventArgs e)
+    {
+        ColorInput.IsEnabled = false;
     }
     
     private void MethodOfGraphInput_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -36,7 +47,11 @@ public partial class MainWindow
     {
         if (ColorLimitCheckbox.IsChecked == true)
         {
-            int colorLimit = int.Parse(ColorInput.Text);
+            if(!int.TryParse(ColorInput.Text, out int colorLimit))
+            {
+                MessageBox.Show("Введіть коректну кількість кольорів");
+                return;
+            }
             if (colorLimit < g.FindChromaticNumberOfGraph())
             {
                 MessageBox.Show("Введена кількість кольорів менша за хроматичне число графа (" + g.FindChromaticNumberOfGraph() + ")");
@@ -54,8 +69,27 @@ public partial class MainWindow
     
     private void StartButton_Click(object sender, RoutedEventArgs e)
     {
-        int size = int.Parse(VertexInput.Text);
+        if(MethodOfGraphInput.SelectedIndex == -1)
+        {
+            MessageBox.Show("Виберіть метод введення графа");
+            return;
+        }
+        if(MethodOfColorInput.SelectedIndex == -1)
+        {
+            MessageBox.Show("Виберіть метод розфарбування");
+            return;
+        }
+        if(!int.TryParse(VertexInput.Text, out int size))
+        {
+            MessageBox.Show("Введіть коректний розмір графа");
+            return;
+        }
         var graphInput = GraphInput.Text;
+        if (string.IsNullOrEmpty(graphInput))
+        {
+            MessageBox.Show("Введіть граф");
+            return;
+        }
         List<List<int>> numbers = graphInput.Split('\n')
             .Select(line => line.Split(' ')
                 .Select(int.Parse)
@@ -68,7 +102,11 @@ public partial class MainWindow
         }
         else
         {
-            int amountOfPairs = int.Parse(EdgeInput.Text);
+            if(!int.TryParse(EdgeInput.Text, out int amountOfPairs))
+            {
+                MessageBox.Show("Введіть коректну кількість пар вершин");
+                return;
+            }
             List<Tuple<int, int> > pairs = new List<Tuple<int, int>>();
             for (int i = 0; i < amountOfPairs; i++)
             {
